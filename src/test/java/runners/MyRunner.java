4496869a74655a764edd353d7e26bf3a62ae3066
Tomek <tomeksochacki@wp.pl>
2024-08-27 12:44:00 +0200

    added parallel execution

--- src/test/java/runners/MyRunner.java
+++ src/test/java/runners/MyRunner.java
@@ -2,10 +2,17 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
                 tags = "@login", monochrome = true, dryRun = false,
                 plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class MyRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
